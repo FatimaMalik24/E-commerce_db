9612885bb0eca1eb8d372521b63835104593815a
FatimaMalik24 <155756516+FatimaMalik24@users.noreply.github.com>
2025-09-10 16:15:06 +0500

    phase 4

---
+++ utils/errors.js
@@ -0,0 +1,9 @@
+class errors extends Error {
+  constructor(message, statusCode = 500, code = "INTERNAL_ERROR") {
+    super(message);
+    this.statusCode = statusCode;
+    this.code = code;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+module.exports = AppError;
